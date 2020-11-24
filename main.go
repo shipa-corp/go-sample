fbf343af4954415040634ac699daf1785f5c4e75
Vivek Pandey <vivek.pandey@gmail.com>
2020-11-23 17:35:21 -0800

    vault annotations

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
     "fmt"
     "net/http"
     "os"
+    "io/ioutil"
 )
 
 func main() {
     http.HandleFunc("/", hello)
-    fmt.Println("listening...")
+    fmt.Println("Listening...")
     err := http.ListenAndServe(":" + os.Getenv("PORT"), nil)
     if err != nil {
         panic(err)
@@ -16,5 +17,13 @@ func main() {
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "Shipa apps")
+    dat, err := ioutil.ReadFile("/vault/secrets/config")
+    if err != nil {
+        fmt.Print(err.Error())
+        fmt.Fprintln(res,string(err.Error()))
+    }
+    fmt.Fprintln(res,string(dat))
+    fmt.Fprintln(res, "Hello from Shipa!")
+    dbName := fmt.Sprintf("DB_NAME: %s", os.Getenv("DB_NAME"))
+    fmt.Fprintln(res, dbName) 
 }
